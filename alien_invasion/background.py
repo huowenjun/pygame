f6a1e71bd3a26ee70057fa342420acd5d2678c8c
hwj <hwj7123@163.com>
2018-12-17 22:25:48 +0800

    固定飞机

--- alien_invasion/background.py
+++ alien_invasion/background.py
@@ -7,4 +7,4 @@ class Background():
         self.background = pygame.image.load('images/background.jpg').convert()# 背景图
         self.screen = screen
     def blitem(self):
-        self.screen.blit(self.background,(0, 0))
+        self.screen.blit(self.background,(0, 0))#blit( )方法，这次是把上一步粘貼好文本的background再拷贝在screen上，（0，0）表示从坐标原点开始就是从左上角开始；
--- alien_invasion/game_function.py
+++ alien_invasion/game_function.py
@@ -1,10 +1,7 @@
 import sys
 import pygame
-def check_events(ship):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
+def check_keydown_events(event,ship):
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
                 #ship.rect.centerx += 1
@@ -17,7 +14,8 @@ def check_events(ship):
             elif event.key == pygame.K_DOWN:
                 ship.moving_bottom = True
                 # ship.rect.bottom += 1
-        elif event.type == pygame.KEYUP:
+def check_keyup_events(event,ship):
+        if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
             elif event.key == pygame.K_LEFT:
@@ -27,7 +25,16 @@ def check_events(ship):
                 # ship.rect.top -= 1
             elif event.key == pygame.K_DOWN:
                 ship.moving_bottom = False
-                # ship.rect.bottom += 1
+                # ship.rect.bottom +=1
+
+def check_events(ship):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,ship)
 def update_screen(ai_settings,screen,ship,background):
     background.blitem() # 屏幕背景
     #screen.fill(ai_settings.bg_color)#背景色
